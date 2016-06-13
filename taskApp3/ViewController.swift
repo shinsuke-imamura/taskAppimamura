68cc08ab38b9d41665617861d11d0c30b721843f
今村　仁亮 <cssi04017@g.nihon-u.ac.jp>
2016-06-13 17:36:22 +0900

    Initial Commit

---
+++ taskApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taskApp3
+//
+//  Created by 今村仁亮 on 2016/06/13.
+//  Copyright © 2016年 shinsuke.imamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
